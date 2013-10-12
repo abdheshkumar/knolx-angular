4764f55bec665df2ac0ac43ca2c9ec649c36f72c
Abdhesh kumar <abdhesh@knoldus.com>
2013-10-12 16:39:16 +0530

    Create AngularJs Application

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('knolxAppApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
